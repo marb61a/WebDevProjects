925e2c764818c34d792e70cda0587b6dcd49b7f6
Martin <marb61a@gmail.com>
2022-08-24 14:45:28 +0100

    Updated user controller

--- ReactFacebook/backend/controllers/user.js
+++ ReactFacebook/backend/controllers/user.js
@@ -1,3 +1,30 @@
-exports.home = (req, res) => {
-    res.send("welcome from user home");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+const { generateToken } = require("../helpers/tokens");
+const { sendVerificationEmail } = require("../helpers/mailer");
+const {
+    validateEmail,
+    validateLength,
+    validateUsername,
+} = require("../helpers/validation");
+
+exports.register = async(req, res) => {
+    try{
+        const {
+            first_name,
+            last_name,
+            email,
+            password,
+            username
+        } = req.body;
+
+        // Check email address
+        if(!validateEmail(email)){
+
+        }
+    }catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
