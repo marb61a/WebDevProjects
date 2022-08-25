488367c4b929b20e135d285995ca0f141dffa2de
Martin <marb61a@gmail.com>
2022-08-25 13:20:11 +0100

    Updated server.js and user routes

--- ReactFacebook/backend/server.js
+++ ReactFacebook/backend/server.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Create express app
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+//routes
+readdirSync("./routes")
+  .map((r) => app.use("/", require("./routes/" + r)));
+
+// Connect to the database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connecting to mongodb", err));
 
 // Select the port for the app to listen on
 const PORT = process.env.PORT || 8000;
