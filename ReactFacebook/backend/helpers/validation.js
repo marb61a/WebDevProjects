41ec6e0df9a7e9e564b3a42348d8f1b58a575a7c
Martin <marb61a@gmail.com>
2022-08-26 14:36:28 +0100

    Updated user validation helper and route controller register function

--- ReactFacebook/backend/helpers/validation.js
+++ ReactFacebook/backend/helpers/validation.js
@@ -3,18 +3,30 @@ const User = require("../models/User");
 exports.validateEmail = (email) => {
     return String(email)
         .toLowerCase()
-        .match()
+        .match(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,12})(\.[a-z]{2,12})?$/);
 };
 
 exports.validatelength = (text, min, max) => {
+    if(text.length > max || text.length < min){
+        return false;
+    }
 
+    return true;
 }
 
 exports.validateUsername = async(username) => {
     let a = false;
 
     do{
+        let check = await User.findOne({ username });
 
+        // Changing username
+        if(check){
+            username += (+new Date() * Math.random()).toString().substring(0, 1);
+            a = true;
+        } else {
+            a = false;
+        }
     } while(a);
 
     return username;
