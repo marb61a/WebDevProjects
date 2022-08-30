dde52c9935cd8951b41ed7998009b6243762e702
Martin <marb61a@gmail.com>
2022-08-30 13:29:23 +0100

    Updated LoginInput input component file

--- ReactFacebook/frontend/src/components/inputs/loginInput/index.js
+++ ReactFacebook/frontend/src/components/inputs/loginInput/index.js
@@ -24,8 +24,30 @@ export default function LoginInput({ placeholder, bottom, ...props }){
                 </div>
             )}
             <input 
-            
+                className={meta.touched && meta.error ? "input_error_border" : ""}
+                type={field.type}
+                name={field.name}
+                placeholder={placeholder}
+                {...field}
+                {...props}
             />
+            {meta.touched && meta.error && bottom && (
+                <div 
+                    className={ desktopView ? "input_error input_error_desktop" : "input_error" } style={{ transform: "translateY(3px)"}}
+                >
+                    {meta.touched && meta.error && <ErrorMessage name={field.name} />}
+                    {meta.touched && meta.error && (
+                        <div
+                            className={ desktopView ? "error_arrow_left" : "error_arrow_bottom" }></div>
+                    )}
+                </div>
+            )}
+            {meta.touched && meta.error && (
+                <i
+                    className="error_icon"
+                    style={{ top: `${!bottom && !desktopView ? "63%" : "15px"}` }}
+                ></i>
+            )}
         </div>
     )
 }
