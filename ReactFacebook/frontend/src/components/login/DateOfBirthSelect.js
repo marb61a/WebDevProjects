485791d2827d426506767869ee167dcd70493ecf
Martin <marb61a@gmail.com>
2022-09-01 12:52:01 +0100

    Updated DateOfBirthSelect login component file

--- ReactFacebook/frontend/src/components/login/DateOfBirthSelect.js
+++ ReactFacebook/frontend/src/components/login/DateOfBirthSelect.js
@@ -25,7 +25,37 @@ export default function DateOfBirthSelect({
             className="reg_grid"
             style={{ marginBottom: `${dateError && !view3 ? "90px" : "0"}` }}
         >
-
+            <select name="bDay" value={bDay} onChange={handleRegisterChange}>
+                {days.map((day, i) => (
+                    <option value={day} key={i}>
+                        {day}
+                    </option>
+                ))}
+            </select>
+            <select name="bMonth" value={bMonth} onChange={handleRegisterChange}>
+                {months.map((month, i) => (
+                    <option value={month} key={i}>
+                        {month}
+                    </option>
+                ))}
+            </select>
+            <select name="bYear" value={bYear} onChange={handleRegisterChange}>
+                {years.map((year, i) => (
+                    <option value={year} key={i}>
+                        {year}
+                    </option>
+                ))}
+            </select>
+            {dateError && (
+                <div
+                    className={!view3 ? "input_error" : "input_error input_error_select_large"}
+                >
+                    <div
+                        className={!view3 ? "error_arrow_bottom" : "error_arrow_left"}
+                    ></div>
+                    {dateError}
+                </div>
+            )}
         </div>
     )
 }
