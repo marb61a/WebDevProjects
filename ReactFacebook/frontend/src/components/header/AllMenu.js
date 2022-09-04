83a39cae5cc74e4a14acecb849eb37df255fe31b
Martin <marb61a@gmail.com>
2022-09-04 19:02:57 +0100

    Added AllMenu header file

---
+++ ReactFacebook/frontend/src/components/header/AllMenu.js
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+
+import { menu, create } from "../../data/allMenu";
+import useClickOutside from "../../helpers/clickOutside";
+import AllMenuItem from "./AllMenuItem";
+
+export default function AllMenu(){
+    return(
+        <div className="all_menu">
+            <div className="all_menu_header">Menu</div>
+            <div className="all_menu_wrap scrollbar">
+                <div className="all_left">
+                    <div className="all_menu_search">
+                        <i className="amm_s_ic"></i>
+                        <input type="text" placeholder="Search Menu" />
+                    </div>
+                    <div className="all_menu_group">
+                        <div className="all_menu_group_header">Social</div>
+                        {menu.slice(0, 6).map((item, i) => (
+                            <AllMenuItem 
+                                name={item.name}
+                                description={item.description}
+                                icon={item.icon}
+                                key={i}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
