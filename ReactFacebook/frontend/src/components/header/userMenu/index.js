5ec52b2a350299124272ceb8096044e1030806d9
Martin <marb61a@gmail.com>
2022-09-05 21:18:04 +0100

    Updated header component userMenu index file

--- ReactFacebook/frontend/src/components/header/userMenu/index.js
+++ ReactFacebook/frontend/src/components/header/userMenu/index.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import DisplayAccessibility from "./DisplayAccessibility";
+import HelpSupport from "./HelpSupport";
+import SettingsPrivacy from "./SettingsPrivacy";
+
 export default function UserMenu({ user }) {
     const [visible, setVisible] = useState(0);
 
@@ -8,13 +12,35 @@ export default function UserMenu({ user }) {
         <div className="mmenu">
             {visible && (
                 <div>
-                    <Link>
-                    
+                    <Link to="/profile" className="mmenu_header hover3">
+                        <img src={user?.picture} alt="" />
+                        <div className="mmenu_col">
+                            <span>
+                                {user?.first_name} {user?.last_name}
+                            </span>
+                            <span>See your profile</span>
+                        </div>
                     </Link>
                     <div className="mmenu_splitter"></div>
-                    <div >
-
+                    <div className="mmenu_item hover3" onClick={() => {setVisible(1)}}>
+                        <div className="small_circle">
+                            <i className="settings_filled_icon"></i>
+                        </div>
+                        <span>Settings & privacy</span>
+                        <div className="rArrow">
+                            <i className="right_icon"></i>
+                        </div>
                     </div>
+                    <div className="mmenu_item hover3" onClick={() => {setVisible(2)}}>
+                        <div className="small_circle">
+                            <i className="help_filled_icon"></i>
+                        </div>
+                        <span>Help & Support</span>
+                        <div className="rArrow">
+                            <i className="right_icon"></i>
+                        </div>
+                    </div>
+                    
                 </div>
             )}
         </div>
