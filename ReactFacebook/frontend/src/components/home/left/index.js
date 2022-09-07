8b28e893283c164a4ba035b90f36832e7f2241bb
Martin <marb61a@gmail.com>
2022-09-07 13:11:34 +0100

    Added home component left index.js page

---
+++ ReactFacebook/frontend/src/components/home/left/index.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./style.css";
+import LeftLink from "./LeftLink";
+import { left } from "../../../data/home";
+import { ArrowDown1 } from "../../../svg";
+import Shortcut from "./Shortcut";
+
+export default function LeftHome({ user}) {
+    const [visible, setVisible] = useState(false);
+
+    return(
+        <div className="left_home scrollbar">
+            <Link to="/profile" className="left_link hover1">
+                <img src={user?.picture} alt="" />
+                <span>
+                    {user?.first_name} {user?.last_name}
+                </span>
+            </Link>
+            {left.slice(0, 8).map((link, i) => (
+                <LeftLink 
+                    key={i}
+                    img={link.img}
+                    text={link.text}
+                    notification={link.notification}
+                />
+            ))}
+        </div>
+    );
+}
