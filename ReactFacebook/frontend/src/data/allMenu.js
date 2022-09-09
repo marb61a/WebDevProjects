59534e2bb29cff61aebc1a749293c1c597ba8c0d
Martin <marb61a@gmail.com>
2022-09-09 13:36:56 +0100

    updated allMenu data file

--- ReactFacebook/frontend/src/data/allMenu.js
+++ ReactFacebook/frontend/src/data/allMenu.js
@@ -114,4 +114,44 @@ export const menu = [
         icon: "messkids",
         description: "Let kids message with close friends and family."
     }
+];
+
+export const create = [
+    {
+        name: "Post",
+        icon: "m_post_icon",
+    },
+    {
+        name: "Story",
+        icon: "m_story_icon"
+    },
+    {
+        name: "Room",
+        icon: "m_room_icon"
+    },
+    {
+        name: "Page",
+        icon: "m_page_icon"
+    },
+
+    {
+        name: "Ad",
+        icon: "m_ad_icon"
+    },
+    {
+        name: "Group",
+        icon: "m_group_icon"
+    },
+    {
+        name: "Event",
+        icon: "m_event_icon"
+    },
+    {
+        name: "Marketplace Listing",
+        icon: "m_post_mar"
+    },
+    {
+        name: "Job",
+        icon: "m_post_job"
+    }    
 ]
