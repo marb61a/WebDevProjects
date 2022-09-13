37fe356bbea8b52aeb41debc8a1616f89b98368b
Martin <marb61a@gmail.com>
2022-09-13 21:53:12 +0100

    Updated app.js and index.js

--- ReactFacebook/frontend/src/index.js
+++ ReactFacebook/frontend/src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+const store = createStore(rootReducer, composeWithDevTools());
+
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById("root")
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+  document.getElementById("root")
 );
