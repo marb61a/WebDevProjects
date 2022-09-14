02f95ed5fdb11a5d93e828be3510faf7cb82ca56
Martin <marb61a@gmail.com>
2022-09-14 14:03:46 +0100

    Updated Image Preview with handleImages function

--- ReactFacebook/frontend/src/components/createPostPopup/ImagePreview.js
+++ ReactFacebook/frontend/src/components/createPostPopup/ImagePreview.js
@@ -10,6 +10,35 @@ export default function ImgaePreview({
     setShowPrev,
     setError
 }){
+    const imageInputRef = useRef(null);
+    const handleImages = (e) => {
+        let files = Array.from(e.target.files);
+        files.forEach((img) => {
+            console.log(img);
+
+            if(
+                img.type !== "image/jpeg" &&
+            img.type !== "image/png" &&
+            img.type !== "image/webp" &&
+            img.type !== "image/gif"
+            ){
+                setError(`${img.name} format is not supported!! only Jpeg, Png, Webp, Gif files are allowed`);
+                files = files.filter((item) => item.name !== img.name);
+                return
+            } else if(img.size > 1024 * 1024){
+                setError(`{img.name} size is too large max 5mb allowed.`);
+                files = files.filter((item) => item.name !== img.name);
+                return;
+            } else {
+                const reader = new FileReader();
+                reader.readAsDataURL(img);
+                reader.onload = (readerEvent) => {
+                    setImages((images) => [...images, readerEvent.target.result]);
+                };
+            }
+        });
+    };
+
     return(
         <div>
 
