e7d19b158bb79485f298a9c060a8058b2337a6ab
Martin <marb61a@gmail.com>
2022-09-14 13:39:00 +0100

    Added createPostPopup component initial ImagePreview.js and PostError.js

---
+++ ReactFacebook/frontend/src/components/createPostPopup/ImagePreview.js
@@ -0,0 +1,18 @@
+import { useRef } from "react";
+import EmojiPickerBackgrounds from "./EmojiPickerBackgrounds";
+
+export default function ImgaePreview({
+    text,
+    user,
+    setText,
+    images,
+    setImages,
+    setShowPrev,
+    setError
+}){
+    return(
+        <div>
+
+        </div>
+    );
+}
