57d1857f23c34bdd43632b8ea95e46a811c04bad
Martin <marb61a@gmail.com>
2022-09-21 13:22:09 +0100

    Updated createPostPopup component index.js

--- ReactFacebook/frontend/src/components/createPostPopup/index.js
+++ ReactFacebook/frontend/src/components/createPostPopup/index.js
@@ -84,7 +84,40 @@ export default function CreatePostPopup({ user, setVisible }){
     return(
         <div className="blur">
             <div className="postBox" ref={popup}>
+                {error && <PostError error={error} setError={setError} />}
+                <div className="box_header">
+                    <div className="small_circle" onClick={() => setVisible()}>
+                        <i className="exit_icon"></i>
+                    </div>
+                    <span>Create Post</span>
+                </div>
+                <div className="box_profile">
+                    <img src={user.picture} alt="" className="box_profile_img" />
+                    <div className="box_col">
+                        <div className="box_profile_name">
+                            {user.first_name} {user.last_name}
+                        </div>
+                        <div className="box_privacy">
+                            <img src="../../../icons/public.png" alt="" />
+                            <span>Public</span>
+                            <i className="arrowDown_icon"></i>
+                        </div>
+                    </div>
+                </div>
+                {!showPrev ? (
+                    <>
+                        <EmojiPickerBackgrounds 
+                            text={text}
+                            user={user}
+                            setText={setText}
+                            showPrev={showPrev}
+                            setBackground={setBackground}
+                            background={background}
+                        />
+                    </>
+                ) : (
 
+                )}
             </div>
         </div>
     );
