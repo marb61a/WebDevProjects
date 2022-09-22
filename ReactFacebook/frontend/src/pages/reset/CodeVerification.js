025c5a42e429da309d3594d93a4b7d68e9a085a7
Martin <marb61a@gmail.com>
2022-09-22 21:50:55 +0100

    Updated reset page CodeVerification file

--- ReactFacebook/frontend/src/pages/reset/CodeVerification.js
+++ ReactFacebook/frontend/src/pages/reset/CodeVerification.js
@@ -16,5 +16,37 @@ export default function CodeVerification({
     setError,
     userInfos,
 }) {
+    const validateCode = Yup.object({
+        code: Yup.string()
+            .required("Code is required")
+            .min("5", "Code must be 5 characters.")
+            .max("5", "Code must be 5 characters.")
+    });
+    const { email } = userInfos;
 
+    const verifyCode = async() => {
+        try{
+            setLoading(true);
+            await axios.post(`${process.env.REACT_APP_BACKEND_URL}/validateResetCode`, { email, code });
+            setVisible(3);
+            setError("");
+            setLoading(false);
+        } catch(error){
+            setLoading(false);
+            setError(error.response.data.message);
+        }
+    };
+
+    return(
+        <div className="reset_form">
+            <div className="reset_form_header">Code verification</div>
+            <div className="reset_form_text">
+                Please enter code that been sent to your email.
+            </div>
+            <Formik
+            >
+
+            </Formik>
+        </div>
+    );
 }
