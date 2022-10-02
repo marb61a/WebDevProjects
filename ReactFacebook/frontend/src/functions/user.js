f556c7ada0ae768a0d6729b1648f34ebaac12e7a
Martin <marb61a@gmail.com>
2022-10-02 17:32:23 +0100

    Updated user functions file with addfriend and cancelRequest functions

--- ReactFacebook/frontend/src/functions/user.js
+++ ReactFacebook/frontend/src/functions/user.js
@@ -25,3 +25,29 @@ export const updateCover = async(url, token) => {
         return error.response.data.message;
     }
 };
+
+export const addFriend = async(id, token) => {
+    try {
+        const { data } = await axios.put(
+            `${process.env.REACT_APP_BACKEND_URL}/addFriend/${id}`, {}, {headers: {Authorization: `Bearer ${token}`}}
+        );
+
+        return "ok";
+
+    } catch (error) {
+        return error.response.data.message;
+    }
+};
+
+export const cancelRequest = async(id, token) => {
+    try {
+        const { data } = await axios.put(
+            `${process.env.REACT_APP_BACKEND_URL}/cancelRequest/${id}`, {}, {headers: {Authorization: `Bearer ${token}`}}
+        );
+
+        return "ok";
+
+    } catch (error) {
+        return error.response.data.message;
+    }
+};
