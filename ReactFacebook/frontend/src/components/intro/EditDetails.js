b4fee8ef4f081aa13469d62b1611f20ea5580169
Martin <marb61a@gmail.com>
2022-10-04 14:19:05 +0100

    Updated intro EditDetails component

--- ReactFacebook/frontend/src/components/intro/EditDetails.js
+++ ReactFacebook/frontend/src/components/intro/EditDetails.js
@@ -17,7 +17,39 @@ export default function EditDetails({
         <div className="blur">
             <div className="postBox infosBox" ref={modal}>
                 <div className="box_header">
-
+                    <div className="small_circle" onClick={() => setVisible(false)}>
+                        <i className="exit_icon"></i>
+                    </div>
+                    <span>Edit Details</span>
+                </div>
+                <div className="details_wrapper scrollbar">
+                    <div className="details_col">
+                        <span>Customize Your Intro</span>
+                        <span>Details you select will be public</span>
+                    </div>
+                    <div className="details_header">Other Name</div>
+                        <Detail 
+                            value={details?.otherName}
+                            img="studies"
+                            placeholder="Add other name"
+                            name="otherName"
+                            text="Other Name"
+                            handleChange={handleChange}
+                            updateDetails={updateDetails}
+                            infos={infos}
+                        />
+                    <div className="details_header">Work</div>
+                        <Detail
+                            value={details?.job}
+                            img="job"
+                            placeholder="Add job title"
+                            name="job"
+                            text="a job"
+                            handleChange={handleChange}
+                            updateDetails={updateDetails}
+                            infos={infos}
+                        />
+                        <Detail />
                 </div>
             </div>
         </div>
