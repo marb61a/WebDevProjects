8d44cf4140f36cc830c088c5ee3d30a2199215ee
Martin <marb61a@gmail.com>
2022-10-05 21:37:06 +0100

    Updated intro component index.js file

--- ReactFacebook/frontend/src/components/intro/index.js
+++ ReactFacebook/frontend/src/components/intro/index.js
@@ -33,16 +33,38 @@ export default function Intro({ detailss, visitor, setOthername }) {
         try{
             console.log("sent");
 
+            const { data } = await axios.put(`${process.enx.REACT_APP_BACKEND_URL}/updateDetails`, {}, { headers: {Authorization: `Bearer ${user.token}`}});
+            setShowBio(false);
+            setDetails(data);
+            setOthername(data.otherName);
         } catch (error) {
             console.log(error.response.data.message);
         }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInfos({ ...infos, [name]: value });
+        setMax(100 - e.target.value.length);
     };
 
     return(
         <div className="profile_card">
             <div className="profile_card_header">Intro</div>
             {details?.bio && !showBio && (
-                <div></div>
+                <div className="info_col">
+                    <span className="info_text">{details?.bio}</span>
+                    {!visitor && (
+                        <button className="gray_btn hover1" onClick={() => setShowBio(true)}>
+                            Edit Bio
+                        </button>
+                    )}
+                </div>
+            )}
+            {!details?.bio && !showBio && !visitor && (
+                <button className="gray_btn hover1 w100" onClick={() => setShowBio(true)}>
+                    Add Bio
+                </button>
             )}
         </div>
     );
