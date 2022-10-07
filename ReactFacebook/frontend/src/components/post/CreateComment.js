1097c2a28655e3ca358f7264d1909d271084fa4c
Martin <marb61a@gmail.com>
2022-10-07 13:16:37 +0100

    Updated post component CreateComment file

--- ReactFacebook/frontend/src/components/post/CreateComment.js
+++ ReactFacebook/frontend/src/components/post/CreateComment.js
@@ -10,9 +10,37 @@ export default function CreateComment({ user }){
     const textRef = useRef(null);
     const imgInput = useRef(null);
 
-    const handleEmoji = (e, { emoji }) => {
+    useEffect(() => {
+        textRef.current.selectionEnd = cursorPosition;
+    }, [cursorPosition]);
 
+    const handleEmoji = (e, { emoji }) => {
+        const ref = textRef.current;
+        ref.focus();
+        const start = text.substring(0, ref.selectionStart);
+        const end = text.substring(ref.selectionStart);
+        const newText = start + emoji + end;
+        setText(newText);
+        setCursorPosition(start.length + emoji.length);
     };
+
+    const handleImage = (e) => {
+        let file = e.target.files[0];
+
+        if(file.type !== "image/jpeg" && file.type !== "image/png" && file.type !== "image/webp" && file.type !== "image/gif"){
+            setError(`${file.name} format is not supported.`);
+            return;
+        } else if (file.size > 1024 * 1024 * 5) {
+            setError(`${file.name} is too large max 5mb allowed.`);
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (event) => {
+            setCommentImage(event.target.result);
+        };
+    }
 
     return(
         <div className="create_comment_wrap">
