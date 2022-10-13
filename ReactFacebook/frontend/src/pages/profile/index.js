aaa990c060526eb56ea3118c8acf780155bae7f0
Martin <marb61a@gmail.com>
2022-10-13 13:13:11 +0100

    Updated profile page index file

--- ReactFacebook/frontend/src/pages/profile/index.js
+++ ReactFacebook/frontend/src/pages/profile/index.js
@@ -8,7 +8,7 @@ import { profileReducer } from "../../functions/reducers";
 import Header from "../../components/header";
 import "./style.css";
 import Cover from "./Cover";
-import ProfielPictureInfos from "./ProfielPictureInfos";
+import ProfilePictureInfos from "./ProfielPictureInfos";
 import ProfileMenu from "./ProfileMenu";
 import PplYouMayKnow from "./PplYouMayKnow";
 import CreatePost from "../../components/createPost";
@@ -30,7 +30,39 @@ export default function Profile({ setVisible }){
             <Header />
             <div className="profile_top" ref={profileTop}>
                 <div className="profile_container">
-
+                    <Cover 
+                        cover={profile.cover}
+                        visitor={visitor}
+                        photos={photos.resources}
+                    />
+                    <ProfilePictureInfos 
+                        profile={profile}
+                        visitor={visitor}
+                        photos={photos.resources}
+                        othername={othername}
+                    />
+                    <ProfileMenu />
+                </div>
+            </div>
+            <div className="profile_bottom">
+                <div className="profile_container">
+                    <div className="bottom_container">
+                        <PplYouMayKnow />
+                        <div
+                            className={`profile_grid ${
+                                check && scrollHeaight >= height && leftHeight > 1000
+                                ? "scrollFixed showLess" : check && scrollHeight >= height && leftHeight < 1000 && "scrollFixed showMore"
+                            }`}
+                        >
+                            <div className="profile_left" ref={leftSide}>
+                                <Intro 
+                                    detailss={profile.details}
+                                    visitor={visitor}
+                                    setOthername={setOthername}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
