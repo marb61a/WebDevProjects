a7ecd25797e507c6e6ff932cf46a4b03022bb547
Martin <marb61a@gmail.com>
2022-10-17 13:20:40 +0100

    Updated profilePicture component index.js

--- ReactFacebook/frontend/src/components/profilePicture/index.js
+++ ReactFacebook/frontend/src/components/profilePicture/index.js
@@ -15,12 +15,41 @@ export default function ProfilePicture({ username, setShow, pRef, photos }){
 
     const handleImage = (e) => {
         let file = e.target.files[0];
-        
-    }
+        if(
+            file.type !== "image/jpeg" && file.type !== "image/png" && file.type !== "image/webp" && file.type !== "image/gif"
+        ){
+            setError(`${file.name} format is not supported.`);
+            return;
+        } else if(file.size > 1024 * 1024 * 5){
+            setError(`${file.name} is too large max 5mb allowed.`);
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (event) => {
+            setImage(event.target.result);
+        };
+    };
 
     return(
         <div className="blur">
-            <input />
+            <input
+                type="file"
+                ref={refInput}
+                hidden
+                onChange={handleImage}
+                accept="image/jpeg,image/png,image/webp,image/gif"
+            />
+            <div className="postBox pictureBox" ref={popup}>
+                <div className="box_header">
+                    <div className="small_circle" onClick={() => setShow(false)}>
+                        <i className="exit_icon"></i>
+                    </div>
+                    <span>Update Profile Picture</span>
+                </div>
+                
+            </div>
         </div>
     );
 }
