0a97d3290403e7ddfe98deb9ef146e27b961634b
Martin <marb61a@gmail.com>
2022-10-18 12:39:57 +0100

    Updated post cmponent index.js file

--- ReactFacebook/frontend/src/components/post/index.js
+++ ReactFacebook/frontend/src/components/post/index.js
@@ -2,7 +2,12 @@ import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { useState } from "react";
 
+import "./style.css";
 import{ Dots, Public} from "../../svg";
+import CreateComment from "./CreateComment";
+import PostMenu from "./PostMenu";
+import ReactsPopup from "./ReactsPopup";
+import { postsReducer } from "../../functions/reducers";
 
 export default function Post({ post, user, profile }){
     const [visible, setVisible] = useState(false);
@@ -17,9 +22,38 @@ export default function Post({ post, user, profile }){
                 >
                     <img src={post.user.picture} alt=""/>
                     <div className="header_col">
-
+                        <div className="post_profile_name">
+                            {post.user.first_name} {post.user.last_name}
+                            <div className="updated_p">
+                                {post.type == "profilePicture" && `
+                                    updated ${post.user.gender === "male" ? "his" : "her"} profile picture
+                                `}
+                                {post.type == "cover picture" && `
+                                    updated ${post.user.gender === "male" ? "his" : "her"} cover picture
+                                `}
+                            </div>
+                        </div>
+                        <div className="post_profile_privacy_date">
+                            <Moment fromNow interval={30}>
+                                {post.createdAt}
+                            </Moment>
+                            . <Public color="#828387" />
+                        </div>
                     </div>
                 </Link>
+                <div
+                    className="post_header_right hover1"
+                    onClick={() => setShowMenu((prev) => !prev)}
+                >
+                    <Dots color="#828387" />
+                </div>
+                {post.background ? (
+                    <div>
+
+                    </div>
+                ) : (<>
+                
+                </>)}
             </div>
         </div>
     );
