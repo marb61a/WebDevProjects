049fb4dda627d6a4be0201225358b69fd8d47dc5
Martin <marb61a@gmail.com>
2022-10-25 13:38:46 +0100

    Updated profile page cover.js file

--- ReactFacebook/frontend/src/pages/profile/Cover.js
+++ ReactFacebook/frontend/src/pages/profile/Cover.js
@@ -11,7 +11,7 @@ import getCroppedImg from "../../helpers/getCroppedImg";
 import OldCovers from "./OldCovers";
 
 export default function Cover({ cover, visitor, photos }){
-    const [showCoverMneu, setShowCoverMenu] = useState(false);
+    const [showCoverMenu, setShowCoverMenu] = useState(false);
     const [coverPicture, setCoverPicture] = useState("");
     const [loading, setLoading] = useState(false);
     const [show, setShow] = useState(false);
@@ -155,7 +155,32 @@ export default function Cover({ cover, visitor, photos }){
             {cover && !coverPicture && (
                 <img src={cover} className="cover" alt="" ref={cRef} />
             )}
-            
+            {!visitor && (
+                <div className="update_cover_wrapper">
+                    <div className="open_cover_update" onClick={() => setShowCoverMenu((prev) => !prev)}>
+                        <i className="camera_filled_icon"></i>
+                        Add Cover Photo
+                    </div>
+                    {showCoverMenu && (
+                        <div className="open_cover_menu" ref={menuRef}>
+                            <div className="open_cover_menu_item hover1" onClick={() => setShow(true)}>
+                                <i className="photo_icon"></i>
+                                Select Photo
+                            </div>
+                            <div className="open_cover_menu_item hover1" onClick={() => refInput.current.click()}>
+                                <i className="upload_icon">Upload Photo</i>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
+            {show && (
+                <OldCovers 
+                    photos={photos}
+                    setCoverPicture={setCoverPicture}
+                    setShow={setShow}
+                />
+            )}
         </div>
     );
 }
