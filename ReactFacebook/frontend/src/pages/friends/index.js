4d46c323920e82b0d09e6b811c8e7cc3594822c5
Martin <marb61a@gmail.com>
2022-10-26 13:38:53 +0100

    Updated friends page index.js

--- ReactFacebook/frontend/src/pages/friends/index.js
+++ ReactFacebook/frontend/src/pages/friends/index.js
@@ -21,12 +21,44 @@ export default function Friends(){
         getData();
     }, []);
 
+    const getData = async() => {
+        dispatch({ type: "FRIENDS_REQUEST" });
+        const data = await getFriendsPageInfos(user.token);
+
+        if (data.status === "ok") {
+            dispatch({ type: "FRIENDS_SUCCESS", payload: data.data });
+        } else {
+            dispatch({ type: "FRIENDS_ERROR", payload: data.data });
+        }
+    };
+
     return(
         <>
             <Header page="friends"/>
             <div className="friends">
                 <div className="friends_left">
-
+                    <div className="friends_left_header">
+                        <h3>Friends</h3>
+                        <div className="small_circle">
+                            <i className="settings_filled_icon"></i>
+                        </div>
+                    </div>
+                    <div className="friends_left_wrap">
+                        <Link
+                            to="/friends"
+                            className={`mmenu_item hover3 ${
+                                type === undefined && "active_friends"
+                            }`}
+                        >
+                            <div className="small_circle">
+                                <i className="friends_home_icon "></i>
+                            </div>
+                            <span>Home</span>
+                            <div className="rArrow">
+                                <i className="right_icon"></i>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
