28faca74b7bd9dbe834640c0023b45f359eefec0
Martin <marb61a@gmail.com>
2022-11-01 13:32:19 +0000

    Updated user controller with getProfile function

--- ReactFacebook/backend/controllers/user.js
+++ ReactFacebook/backend/controllers/user.js
@@ -255,3 +255,47 @@ exports.changePassword = async(req, res) => {
     );
     return res.status(200).json({ message: "ok" });
 };
+
+exports.getProfile = async(req, res) => {
+    try{
+        const { username } = req.params;
+        const user = await User.findById(req.user.id);
+        const profile = await User.findOne({ username }).select("-password");
+        const friendship = {
+            friends: false,
+            following: false,
+            requestSent: false,
+            requestReceived: false
+        };
+
+        if(!profile){
+            return res.json({ ok: false });
+        }
+        if(user.friends.includes(profile._id) && profile.friends.includes(user._id)){
+            friendship.friends = true;
+        }
+        if (user.following.includes(profile._id)) {
+            friendship.following = true;
+        }
+        if (user.requests.includes(profile._id)) {
+            friendship.requestReceived = true;
+        }
+        if (profile.requests.includes(user._id)) {
+            friendship.requestSent = true;
+        }
+      
+        const posts = await Post.find({ user: profile._id })
+            .populate("user")
+            .populate(
+                "comments.commentBy",
+                "first_name last_name picture username commentAt"
+            )
+            .sort({ createdAt: -1 });
+        await profile.populate("friends", "first_name last_name username picture");
+        res.json({ ...profile.toObject(), posts, friendship });
+    }catch(error){
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
