41f60c7c3cb985fd1d543ca743dffef71923ccb7
Martin <marb61a@gmail.com>
2022-11-15 13:49:39 +0000

    Updated home page index and ActivateForm files

--- ReactFacebook/frontend/src/pages/home/index.js
+++ ReactFacebook/frontend/src/pages/home/index.js
@@ -17,12 +17,24 @@ export default function Home({ setVisible, posts, loading, getAllPosts }){
     }, [loading, height]);
 
     return(
-        <div className="home">
-            <Header />
+        <div className="home" style={{ height: `${height + 150}px` }}>
+            <Header page="home" getAllPosts={getAllPosts} />
             <LeftHome user={user}/>
-            <div className="home_middle">
+            <div className="home_middle" ref={middle}>
                 <Stories />
-                <CreatePost user={user} />
+                {user.verified === false && <SendVerification user={user} />}
+                <CreatePost user={user} setVisible={setVisible}/>
+                {loading ? (
+                    <div className="sekelton_loader">
+                        <HashLoader color="#1876f2" />
+                    </div>
+                ) : (
+                    <div className="posts">
+                        {posts.map((post, i) => (
+                            <Post key={i} post={post} user={user} />
+                        ))}
+                    </div>
+                )}
             </div>
             <RightHome user={user} />
         </div>
